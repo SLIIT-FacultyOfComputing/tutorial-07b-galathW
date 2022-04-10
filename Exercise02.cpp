15cb75d544c4ba966fa50e43b341b90f14fef17b
Venura Kaushan <95140756+galathW@users.noreply.github.com>
2022-04-10 19:00:44 +0000

    Done the all

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -15,14 +15,21 @@ int main() {
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
    // 1. Create a dynamic Rectangle type variable (pointer)
+  Rectangle *rec;
    // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+  rec = new Rectangle(length,width);
    // 3. Create a dynamic Circle type variable (pointer)
+  Circle *cir;
    // 4. Create a dynamic Circle Object set radius that was input from the keyboard
+  cir = new Circle(radius);
    // 5. Call the display method of the Rectangle Object
+  rec->display();
    // 6. Call the display method of the Circle Object
+  cir->display();
    // 7. Delete the Rectangle Object from memory
+  delete rec;
    // 8. Delete the Circle Object from memory
-  
+  delete cir;
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
    cout << "End of Program" << endl;
--- Item.cpp
+++ Item.cpp
@@ -3,9 +3,30 @@
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
+Item::Item()
+{
+  itemCode = 0;
+  unitPrice = 0;
+  discount = 0;
+}
 // 5. Implement Overloaded Constructor Implementation
+Item::Item(int pItemCode,float pUnitPrice)
+{
+  itemCode = pItemCode;
+  unitPrice = pUnitPrice;
+}
 // 6. Implement Destructor (display "Destructor Called")
-
+Item::~Item()
+{
+  if(itemCode == 0 && unitPrice == 0 && discount == 0)
+  {
+    cout<<"Destructor Called myItem object "<<endl;
+  }
+  else 
+  {
+    cout<<"Destructor Called myItem2 object "<<endl;
+  }
+}
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
 }
